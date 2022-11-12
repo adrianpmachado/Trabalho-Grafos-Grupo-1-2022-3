080d2f8bd9345988cf3cc3f520a9047572c33ccb
Adrian <machadopereiraadrian@gmail.com>
2022-11-12 17:58:10 -0300

    "Implementação aresta"

--- src/Aresta.cpp
+++ src/Aresta.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
-Aresta::Aresta(int id_saida, int id_destino, bool direcionado, float peso){
-    
+Aresta::Aresta(int id_saida, int id_destino, bool eh_direcionado, float peso){
+    this->id_saida = id_saida;
+    this->id_destino = id_destino;
+    this->eh_direcionado = eh_direcionado;
+    this->peso = peso;
+}
+
+float Aresta::get_peso(){
+    return this->peso;
+}
+int Aresta::get_id_saida(){
+    return this->id_saida;
+}
+int Aresta::get_id_destino(){
+    return this->id_destino;
+}
+float Aresta::get_peso(){
+    return this->peso;
+}
+bool Aresta::get_eh_direcionado(){
+    return this->eh_direcionado;
+}
+
+void Aresta::set_peso(float peso){
+    this->peso=peso;
+}
+void Aresta::set_id_saida(int id_saida){
+    this->id_saida = id_saida;
+}
+void Aresta::set_id_destino(int id_destino){
+    this->id_destino = id_destino;
+}
+void Aresta::set_eh_direcionado(bool eh_direcionado){
+    this->eh_direcionado = eh_direcionado;
 }
--- src/Aresta.h
+++ src/Aresta.h
@@ -5,18 +5,20 @@ private:
     float peso;
     int id_saida;
     int id_destino;
-    float peso;
+    bool eh_direcionado;
 public:
-    Aresta(int id_saida, int id_destino, bool direcionado, float peso);
+    Aresta(int id_saida, int id_destino, bool eh_direcionado, float peso);
     ~Aresta();
 
     //metodos padrões
     float get_peso();
     int get_id_saida();
     int get_id_destino();
+    bool get_eh_direcionado();
 
-    void set_peso();
-    void set_id_aresta();
-    void set_id_proxima_aresta();
+    void set_peso(float peso);
+    void set_id_saida(int id_saida);
+    void set_id_destino(int id_destino);
+    void set_eh_direcionado(bool eh_direcionado);
 
 };
