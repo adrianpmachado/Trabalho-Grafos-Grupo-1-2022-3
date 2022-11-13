11c457c46cbfdd942ba63a65cc4f1873d490492d
Adrian <machadopereiraadrian@gmail.com>
2022-11-13 19:18:55 -0300

    Inserindo Aresta não direcionada

--- src/Soucers/grafo.cpp
+++ src/Soucers/grafo.cpp
@@ -45,15 +45,38 @@ void Grafo::insere_aresta(int id_saida,int id_destino, bool direcionado, float p
     if(!vertices_grafo.empty() && existe_vertice(id_saida) && existe_vertice(id_destino)){
         //verificar se existe algum vertice no grafo, se existir verificar e existe o de saida e o de destino em especifico
         if(direcionado){
-            Vertice* vertice_aux_01 = busca_vertice(id_saida);
-            Vertice* vertice_aux_02 = busca_vertice(id_destino);
+            //continuar
                        
         }
         else{
-            
+            //criando um ponteiro para um vertice para podermos manipular o vertice de saida
+            Vertice* vertice_aux_01 = this->busca_vertice(id_saida);
+            //criand um ponteiro para um vettice para podermos manipular o vertice de entrada
+            Vertice* vertice_aux_02 = this->busca_vertice(id_destino);
+            //criando um vertice auxiliar para podemos depois salvar na lista de arestas do grafo
+            Aresta* aresta_aux_01;
+            //inserindo uma aresta não direcinonada no vertice
+            aresta_aux_01 = vertice_aux_01->insere_aresta_nao_direcionada(id_saida,id_destino,peso);
+            //adicionando +1 ao grau de saida e entrada, já que direcionado == 0
+            vertice_aux_01->set_grau_saida(vertice_aux_01->get_grau_saida()+1);
+            vertice_aux_01->set_grau_entrada(vertice_aux_01->get_grau_entrada()+1);
+            //adicionando +1 ao grau de saida e entrada, já que não é direcionado
+            vertice_aux_02->set_grau_saida(vertice_aux_02->get_grau_saida()+1);
+            vertice_aux_02->set_grau_entrada(vertice_aux_02->get_grau_entrada()+1);
+            //adicionando a aresta na lista de arestas do grafo
+            this->arestas_grafo.push_back(aresta_aux_01);
         }
     }
 }
 bool Grafo::existe_Aresta(int id_saida, int id_destino, bool direcionado, float peso){
-    
+    if(direcionado){
+        //continuar
+    }
+    else{
+        for(Aresta* aresta_aux :arestas_grafo){
+            if((aresta_aux->get_id_saida()==id_saida)&&(aresta_aux->get_id_destino()==id_destino))
+                return true;
+        }
+        return false;
+    }
 }
