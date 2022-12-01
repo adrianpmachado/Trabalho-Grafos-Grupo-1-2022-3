b00909fe5d8f36e375dedb2006e408464000296c
Adrian Pereira Machado <machadopereiraadrian@gmail.com>
2022-12-01 16:38:38 -0300

    teste leitura

--- main.cpp
+++ main.cpp
@@ -13,11 +13,15 @@ int main (int argc, char * argv[])
     //./execGrupo1 "instancias Grafos não ponderados/grafo_1000_1.txt" arquivo.dot 0 0 0
     //./execGrupoX <arquivo_entrada> <arquivo_saida> <Opc_Direc> <Opc_Peso_Aresta> <Opc_Peso_Nos>,
 
+    bool Opc_Direc;
+    bool Opc_Peso_Aresta;
+    bool Opc_Peso_Nos;
+
     string caminho_entrada = argv [1];
     string caminho_saida = argv[2];
-    bool Opc_Direc = istringstream(argv[3]);
-    bool Opc_Peso_Aresta = istringstream(argv[4]);
-    bool Opc_Peso_Aresta = istringstream(argv[5]);
+    istringstream(argv[3])>>Opc_Direc;
+    istringstream(argv[4])>>Opc_Peso_Aresta;
+    istringstream(argv[5])>>Opc_Peso_Nos;
     Grafo* grafo01 = new Grafo(caminho_entrada,0,0,0);
     grafo01->salva_grafo(caminho_saida);
     return 0;
