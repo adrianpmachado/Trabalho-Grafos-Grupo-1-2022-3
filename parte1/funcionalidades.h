2030282162fd3aee70df47c540501043e3eaed8d
Mário Neto <85771430+Mariojose006@users.noreply.github.com>
2023-01-07 13:23:06 -0300

    grafo-intercesao

--- parte1/funcionalidades.h
+++ parte1/funcionalidades.h
@@ -36,6 +36,21 @@ Grafo *obter_novo_grafo(bool direcionado, bool peso_aresta, bool peso_vertice)
 
 void grafo_intersecao(Grafo *grafo, string caminho_saida)
 {
+	Grafo *novo_grafo = obter_novo_grafo(grafo->obter_direcionado(), grafo->obter_peso_vertice(), grafo->obter_peso_aresta());
+
+	cout << "Intersecão finalizada." << endl;
+	cout << "\nGrafo resultado:\n";
+	
+	Grafo *intersecao;
+	for (auto aresta_aux : novo_grafo->arestas_grafo)
+	{
+		if(grafo->existe_Aresta(aresta_aux->obter_id_destino(),aresta_aux->obter_id_saida())){
+			intersecao->insere_aresta(aresta_aux->obter_id_saida(), aresta_aux->obter_id_destino(), aresta_aux->obter_peso());
+		}
+	}
+	
+	grafo->imprimir_grafo_lista_de_adjacencia();
+	cout << endl;
 	grafo->salva_grafo(caminho_saida);
 
 	return;
